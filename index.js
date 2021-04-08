bf46f233124c4708b1e6a9504f2f11ed0447e5d1
jazzy <jazzy@kali.kali>
2021-04-07 20:45:21 -0600

    I have successfully communicated from node js server to react app. Still need to take that data from a fetch to jsx

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const bodyParser = require('body-parser');
 const urlEncoded = bodyParser.urlencoded({extended: false});
 
 app.use(express.static('assets'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', '*');
+    res.status(200).json({});
+  }
+
+  next();
+})
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/login.html`);
@@ -20,12 +31,16 @@ app.post('/login', urlEncoded, (req, res) => {
   console.log(req.body)
 })
 
+app.get('/api', urlEncoded, (req, res) => {
+  res.json({user: 'KillerPuppy2000'});
+})
+
 
 
 
 
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Listening to ${port}...`)
+app.listen(3001, () => {
+  console.log(`Listening to 3001...`)
 })
--- react-frontend/src/FetchApi.js
+++ react-frontend/src/FetchApi.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const FetchApi = () => {
-
    const apiGet = () => {
-    fetch('http://localhost:3001')
+    fetch('http://localhost:3001/api'
+    )
     .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((jsonRes) => jsonRes)
   }
 
   return(
+
     <div>
       <h1>My Api</h1>
 
       <button onClick={apiGet}>Fetch Api</button>
+      {/*<div>{data}</div>*/}
     </div>
   )
 }
